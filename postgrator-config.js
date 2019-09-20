b8d7ace354e10f934159de92cdc0639e6255128f
Jess Doyle <jessica.e.doyle@gmail.com>
2019-09-20 11:01:50 -0400

    updated migration files for deployment

---
+++ postgrator-config.js
@@ -0,0 +1,9 @@
+'use strict';
+require('dotenv').config();
+
+module.exports = {
+  "migrationsDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
+}
